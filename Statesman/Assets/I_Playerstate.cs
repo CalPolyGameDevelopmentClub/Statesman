359bffb4f2a6ab15645666b9f02c350991a0c588
Kyle Piddington <kpiddy@gmail.com>
2015-04-23 12:52:18 -0700

    Initial Files

---
+++ Statesman/Assets/I_Playerstate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface I_Playerstate {
+
+	void onEnter(Transform player);
+	void onExit (Transform player);
+
+	// Update is called once per frame
+	void Update (Transform player, float dt);
+	void handleInput(Transform player);
+}
